5bf86d4e807092ea44fac0eb5a6b664fd46673f5
neelgohel <nealgohel7@yahoo.com>
2017-03-22 16:10:44 +0530

    invalid date for booking will not be accepted.

--- Homework/app/controllers/bookings_controller.rb
+++ Homework/app/controllers/bookings_controller.rb
@@ -15,23 +15,24 @@ class BookingsController < ApplicationController
     @city = City.all
     @booking = Booking.new(booking_params)
     @booking.customer_id = current_customer.id
-    datetime = Time.local(params[:booking]["datetime(1i)"].to_i, params[:booking]["datetime(2i)"].to_i,
-                        params[:booking]["datetime(3i)"].to_i, params[:booking]["datetime(4i)"].to_i,
-                        params[:booking]["datetime(5i)"].to_i)
-    city = City.find(params[:booking][:city_id])
-    city_bookings_at_same_time = city.bookings.where(datetime:datetime)
-    cleaners_not_available = []
-    city_bookings_at_same_time.each do |booking|
-      cleaners_not_available.push(booking.cleaner.id)
-    end
-    cleaners_in_city = city.cleaners.where.not(id:cleaners_not_available)
-    cleaners_available = []
-    cleaners_in_city.each do |cleaner|
-      if cleaner.bookings.where(datetime:((datetime-2.hours)..(datetime+2.hours))) == []
-        cleaners_available.push(cleaner.id)
+    begin
+      datetime = DateTime.new(params[:booking]["datetime(1i)"].to_i, params[:booking]["datetime(2i)"].to_i,
+                          params[:booking]["datetime(3i)"].to_i, params[:booking]["datetime(4i)"].to_i,
+                          params[:booking]["datetime(5i)"].to_i)
+      city = City.find(params[:booking][:city_id])
+      city_bookings_at_same_time = city.bookings.where(datetime:datetime)
+      cleaners_not_available = []
+      city_bookings_at_same_time.each do |booking|
+        cleaners_not_available.push(booking.cleaner.id)
       end
-    end
-    if cleaners_available != []
+      cleaners_in_city = city.cleaners.where.not(id:cleaners_not_available)
+      cleaners_available = []
+      cleaners_in_city.each do |cleaner|
+        if cleaner.bookings.where(datetime:((datetime-2.hours)..(datetime+2.hours))) == []
+          cleaners_available.push(cleaner.id)
+        end
+      end
+      if cleaners_available != []
       @booking.cleaner_id = cleaners_available.sample
       if @booking.save
         BookingmailMailer.mail_cleaner(@booking.id).deliver_later
@@ -39,8 +40,12 @@ class BookingsController < ApplicationController
       else
         render 'new'
       end
-    else
-      flash[:alert] = "No cleaner available at given time.."
+      else
+        flash[:alert] = "No cleaner available at given time.."
+        redirect_to new_booking_path
+      end
+    rescue
+      flash[:alert] = "Invalid Date."
       redirect_to new_booking_path
     end
   end
